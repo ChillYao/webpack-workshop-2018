0dc9b6d3ddcc9a13273bf39e03152e7127590613
tong.yao <tong.yao@check24.de>
2022-12-09 17:01:50 +0100

    setting up local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  mode: "none"
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env) => {
+  return {
+    mode: env.mode,
+    output: {
+      filename: "budnle.js",
+    },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+  };
 };
