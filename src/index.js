2b62859ee5d0e1e66da4edd0262dfc626fb3608a
tong.yao <tong.yao@check24.de>
2022-12-12 18:44:25 +0100

    limit file size complete

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import makeImage from "./image";
+import imageUrl from "./Grundriss.jpeg";
+
+const image = makeImage(imageUrl);
 
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 document.body.appendChild(button);
 document.body.appendChild(footer);
+document.body.appendChild(image);
